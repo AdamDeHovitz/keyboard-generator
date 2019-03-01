badbf8674fb2334543221c55dbc7e6500bb720e1
marko-fejzo <markos.business@gmail.com>
2019-03-01 15:26:11 -0500

    lemonke lmfao

---
+++ frequency_generator.py
@@ -0,0 +1,20 @@
+import os
+from collections import defaultdict
+
+MAX_N = 3
+CORPORA_PATH = './corpora/'
+
+freqs = []
+for i in range(MAX_N):
+	freqs.append(defaultdict(int))
+
+for file in os.listdir(CORPORA_PATH):
+    if file.endswith(".txt"):
+        with open(CORPORA_PATH + file, 'r') as f:
+        	contents = f.read()
+        	for i in range(len(contents)):
+        		for j in range(1, MAX_N+1):
+        			if i + j <= len(contents) and contents[i:i+j].isalpha():
+        				freqs[j-1][contents[i:i+j]] += 1
+
+print(freqs)
