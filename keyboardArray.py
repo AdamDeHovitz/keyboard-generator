660aeb13e75d00d6603f75996cecb9d004c12b35
marko-fejzo <markos.business@gmail.com>
2019-03-08 15:15:53 -0500

    refactored saf's shit code

--- keyboardArray.py
+++ keyboardArray.py
@@ -1,81 +1,71 @@
+from namedlist import namedlist
 
-
-keys = []
+Key = namedlist('Key', ['row', 'finger'])
+keys = [Key(0, 0) for i in range(26)]
 for i in range(10):
-	keys.append(10)
+	keys[i].row = 1
 for i in range(10, 19):
-	keys.append(20)
+	keys[i].row = 2
 for i in range(19, 26):
-	keys.append(30)
-keys[0] += 1 #q
-keys[10] += 1 #a
-keys[19] += 1 #z
+	keys[i].row = 3
+keys[0].finger = 1 #q
+keys[10].finger = 1 #a
+keys[19].finger = 1 #z
 
-keys[1] += 2 #w
-keys[11] += 2 #s
-keys[20] += 2 #x
+keys[1].finger = 2 #w
+keys[11].finger = 2 #s
+keys[20].finger = 2 #x
 
-keys[2] += 3 #e
-keys[12] += 3 #d
-keys[21] += 3 #c
+keys[2].finger = 3 #e
+keys[12].finger = 3 #d
+keys[21].finger = 3 #c
 
-keys[3] += 4 #r
-keys[13] += 4 #f
-keys[22] += 4 #v
+keys[3].finger = 4 #r
+keys[13].finger = 4 #f
+keys[22].finger = 4 #v
 
-keys[4] += 4 #t
-keys[14] += 4 #g
-keys[23] += 4 #b
+keys[4].finger = 4 #t
+keys[14].finger = 4 #g
+keys[23].finger = 4 #b
 
-keys[5] += 5 #y
-keys[15] += 5 #h
-keys[24] += 5 #n
+keys[5].finger = 5 #y
+keys[15].finger = 5 #h
+keys[24].finger = 5 #n
 
-keys[6] += 5 #u
-keys[16] += 5 #j
-keys[25] += 5 #m
+keys[6].finger = 5 #u
+keys[16].finger = 5 #j
+keys[25].finger = 5 #m
 
-keys[7] += 6 #i
-keys[17] += 6 #
+keys[7].finger = 6 #i
+keys[17].finger = 6 #
 
-keys[8] += 7 #o
-keys[18] += 7 #l
+keys[8].finger = 7 #o
+keys[18].finger = 7 #l
 
-keys[9] += 8 #p
+keys[9].finger = 8 #p
 
-keyDistances = [0]*26
+keyDistances = [[] for _ in range(26)]
 
 for i in range(26):
-	keyDistances[i] = []
-	fingerI = keys[i] % 10
-	rowI = (keys[i] - fingerI) / 10
-
 	for j in range(26):
 		keyDistances[i].append(0)
-		fingerJ = keys[j] % 10
-		rowJ = (keys[j] - fingerJ) / 10
-
 		if i == j:
 			#Same Key
 			keyDistances[i][j] = 0 
-		elif fingerI == fingerJ:
+		elif keys[i].finger == keys[j].finger:
 			#Same finger used to type these keys
-			if (rowI == 1 and rowJ == 3) or (rowI == 3 and rowJ == 1):
+			if (keys[i].row == 1 and keys[j].row == 3) or (keys[i].row == 3 and keys[j].row == 1):
 				keyDistances[i][j] = 2
 			else :
 				keyDistances[i][j] = 1
 		else:
 			#Different fingers type these keys. Base off row of j
-			if rowJ == 1:
+			if keys[j].row == 1:
 				#Top Row
 				keyDistances[i][j] = 1
-			elif rowJ == 3:
+			elif keys[j].row == 3:
 				#Bot Row
 				keyDistances[i][j] = 1
 			else:
 				#Home Row
 				keyDistances[i][j] = 0
- 
-
-			
-
