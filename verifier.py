933d0a00ca5b166fd23f6cc69854c569e6d75a0e
marko-fejzo <markos.business@gmail.com>
2019-05-15 18:20:28 -0400

    added printout of whats missing

--- verifier.py
+++ verifier.py
@@ -1,6 +1,4 @@
 from key_timer import letters
-print('Enter your corpus file path: ')
-f = input()
 s = set()
 
 with open('corpora/great_expectations.txt') as g:
@@ -9,13 +7,20 @@ with open('corpora/great_expectations.txt') as g:
 			pair = (line[i], line[i+1])
 			s.add(pair)
 
-with open(f) as f:
-	for line in f:
-		for i in range(len(line)-1):
-			pair = (line[i], line[i+1])
-			if pair in s:
-				s.remove(pair)
-		if len(s) == 0:
-			print('Corpus has all pairs')
-			exit()
-print('Corpus does not have all pairs')
+def verify(pairs):
+	return set(pairs) == s
+
+if __name__ == "__main__":
+	print('Enter your corpus file path: ')
+	f = input()
+	with open(f) as f:
+		for line in f:
+			for i in range(len(line)-1):
+				pair = (line[i], line[i+1])
+				if pair in s:
+					s.remove(pair)
+			if len(s) == 0:
+				print('Corpus has all pairs')
+				exit()
+	print('Corpus does not have all pairs, this is what\'s missing: ')
+	print(s)
