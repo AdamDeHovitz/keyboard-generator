065a20da74f11975cf106166dbe0a72dce411c62
Jake Saferstein <jakesafo@gmail.com>
2019-05-15 22:42:26 -0400

    Update

--- verifier.py
+++ verifier.py
@@ -11,6 +11,19 @@ with open('corpora/great_expectations.txt') as g:
 def verify(pairs):
 	return set(pairs) == s
 
+def verify_with_missing(pairs):
+
+	for pair in range(len(pairs)-1):
+		if pair in s:
+			s.remove(pair)
+	if len(s) == 0:
+		return True
+
+	print('Corpus does not have all pairs, this is what\'s missing: ')
+	print(s)
+
+	return False
+
 if __name__ == "__main__":
 	print('Enter your corpus file path: ')
 	f = input()
